ec47c0ebba9be549cfe6b780d5be55ad66ab9582
unknown <mehedytouhidurzaman@gmail.com>
2024-09-24 12:55:42 +0600

    connect mongodb successfully

--- index.js
+++ index.js
@@ -2,11 +2,51 @@ const express = require('express')
 
 const app = express()
 
+require("dotenv").config()
+
 const port = process.env.PORT || 5000
 
 app.get('/', (req,res)=>{
     res.send("coffee is coming soon")
 })
+
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8aq9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
 
 app.listen(port, ()=>{
     console.log(`The Coffee server is running on the port: ${port}`)
